5ab9c46346c90c6a63cecf9ac43bb677b54f8549
BalikaRahul <rr7511489@gmail.com>
2026-06-12 19:25:19 +0530

    automated slug in category model and created store model

--- Backend/webSite/category/admin.py
+++ Backend/webSite/category/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 from .models import Category
 
 # Register your models here.
-admin.site.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields={'slug':('category_name',)}
+    list_display =('category_name','slug') # this to auto fill the slug when enter the category of the product
+admin.site.register(Category,CategoryAdmin)
