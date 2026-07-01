20d1aebcfee52648cc8bf3643e96c93e332d4463
BalikaRahul <rr7511489@gmail.com>
2026-07-01 23:04:47 +0530

    registration form

--- Backend/webSite/accounts/forms.py
+++ Backend/webSite/accounts/forms.py
@@ -2,8 +2,17 @@ from django import forms
 from .models import Account
 
 class RegistrationForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder':'Enter Password'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder':'Enter Password','class':'form-control', }))
     confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder':'confirm Password'}))
     class Meta:
         model=Account
         fields = ['first_name','last_name','phone_number','email','password']
+
+    def __init__(self,*args,**kwargs):
+        super(RegistrationForm,self).__init__(*args,**kwargs)
+        self.fields['first_name'].widget.attrs['placeholder'] ='Enter First_name'
+        self.fields['last_name'].widget.attrs['placeholder'] ='Enter last_name'
+        self.fields['phone_number'].widget.attrs['placeholder'] ='Enter phone_number'
+        self.fields['email'].widget.attrs['placeholder'] ='Enter Email'
+        for field in self.fields:
+            self.fields[field].widget.attrs['class'] ='form-control'
--- Backend/webSite/accounts/views.py
+++ Backend/webSite/accounts/views.py
@@ -1,16 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import RegistrationForm
+from .models import Account
 
-# Create your views here.
+
 def register(request):
-    form = RegistrationForm()
+    if request.method == "POST":
+        form = RegistrationForm(request.POST)
+
+        if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            phone_number = form.cleaned_data['phone_number']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+
+            username = email.split("@")[0]
+
+            user = Account.objects.create_user(
+                first_name=first_name,
+                last_name=last_name,
+                email=email,
+                username=username,
+                phone_number= phone_number,
+                password=password,
+            )
+
+            return redirect('login')
+
+    else:
+        form = RegistrationForm()
+
     context = {
-        'form':form,
+        'form': form,
     }
-    return render(request,'accounts/register.html',context)
+
+    return render(request, 'accounts/register.html', context)
+
 
 def login(request):
-    return render(request,'accounts/login.html')
+    return render(request, 'accounts/login.html')
+
 
 def logout(request):
-    return render(request,'accounts/logout.html')
+    return render(request, 'accounts/logout.html')
